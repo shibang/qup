01023cf39c615a8a6f5e5aaa97fc0ea4acd39f2c
石棒 <shibang@BangsMac.local>
2023-03-16 17:39:00 +0800

    update qiniu dependencies

--- go.mod
+++ go.mod
@@ -3,11 +3,7 @@ module qup
 go 1.12
 
 require (
-	github.com/atotto/clipboard v0.1.2
-	github.com/qiniu/api.v7 v7.2.5+incompatible
-	github.com/qiniu/x v7.0.8+incompatible // indirect
+	github.com/atotto/clipboard v0.1.4
+	github.com/qiniu/go-sdk/v7 v7.14.0
 	github.com/shibang/pb v0.0.0-20190704170843-29a4012fc3bd
-	github.com/stretchr/testify v1.3.0 // indirect
-	golang.org/x/net v0.0.0-20190628185345-da137c7871d7 // indirect
-	qiniupkg.com/x v7.0.8+incompatible // indirect
 )
--- main.go
+++ main.go
@@ -1,113 +1,113 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "log"
-    "net/url"
-    "os"
-    "path/filepath"
-    "strings"
+	"context"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
 
-    "github.com/atotto/clipboard"
-    "github.com/shibang/pb"
-    "github.com/qiniu/api.v7/auth/qbox"
-    "github.com/qiniu/api.v7/storage"
+	"github.com/atotto/clipboard"
+	"github.com/qiniu/go-sdk/v7/auth"
+	"github.com/qiniu/go-sdk/v7/storage"
+	"github.com/shibang/pb"
 )
 
 // UpHost 上传域名，不要改
 const UpHost = "http://up.qbox.me"
 
 var (
-    Bucket string
-    Domain string
-    AK     string
-    SK     string
+	Bucket string
+	Domain string
+	AK     string
+	SK     string
 )
 
 func init() {
-    Bucket = os.Getenv("Q_Bucket")
-    Domain = os.Getenv("Q_Domain")
-    AK = os.Getenv("Q_AK")
-    SK = os.Getenv("Q_SK")
+	Bucket = os.Getenv("Q_Bucket")
+	Domain = os.Getenv("Q_Domain")
+	AK = os.Getenv("Q_AK")
+	SK = os.Getenv("Q_SK")
 }
 
 func main() {
-    if len(os.Args) < 2 {
-        fmt.Printf("Usage: %s <file> [new-name]\n", os.Args[0])
-        return
-    }
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage: %s <file> [new-name]\n", os.Args[0])
+		return
+	}
 
-    if Bucket == "" || Domain == "" || AK == "" || SK == "" {
-        fmt.Println("Please set Q_Bucket, Q_Domain, Q_AK, Q_SK environment variable")
-    }
+	if Bucket == "" || Domain == "" || AK == "" || SK == "" {
+		fmt.Println("Please set Q_Bucket, Q_Domain, Q_AK, Q_SK environment variable")
+	}
 
-    fullName := os.Args[1]
-    f, err := os.Open(fullName)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer f.Close()
+	fullName := os.Args[1]
+	f, err := os.Open(fullName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
 
-    stat, err := f.Stat()
-    if err != nil {
-        log.Fatal(err)
-    }
+	stat, err := f.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    filename := filepath.Base(fullName)
-    if len(os.Args) > 2 {
-        filename = os.Args[2]
-    }
+	filename := filepath.Base(fullName)
+	if len(os.Args) > 2 {
+		filename = os.Args[2]
+	}
 
-    policy := storage.PutPolicy{
-        Scope:   Bucket + ":" + filename,
-        Expires: 360000,
-    }
-    mac := qbox.NewMac(AK, SK)
-    token := policy.UploadToken(mac)
+	policy := storage.PutPolicy{
+		Scope:   Bucket + ":" + filename,
+		Expires: 360000,
+	}
+	mac := auth.New(AK, SK)
+	token := policy.UploadToken(mac)
 
-    storage.SetSettings(&storage.Settings{
-        ChunkSize: 1024 * 1024 * 4,
-    })
+	storage.SetSettings(&storage.Settings{
+		ChunkSize: 1024 * 1024 * 4,
+	})
 
-    conf := &storage.Config{
-        UpHost: UpHost,
-    }
+	conf := &storage.Config{
+		UpHost: UpHost,
+	}
 
-    tmpl := `{{ green "⏳ Uploading:" }} {{counters . }} {{ bar . "[" "=" (cycle . ">" ) " " "]"}} {{percent .}} {{speed .}}`
-    bar := pb.New64(stat.Size())
-    bar.Set(pb.Bytes, true)
-    bar.SetTemplateString(tmpl)
-    if err = bar.Err(); err != nil {
-        return
-    }
-    bar.Start()
+	tmpl := `{{ green "⏳ Uploading:" }} {{counters . }} {{ bar . "[" "=" (cycle . ">" ) " " "]"}} {{percent .}} {{speed .}}`
+	bar := pb.New64(stat.Size())
+	bar.Set(pb.Bytes, true)
+	bar.SetTemplateString(tmpl)
+	if err = bar.Err(); err != nil {
+		return
+	}
+	bar.Start()
 
-    notifyCallback := func(blkIdx int, blkSize int, ret *storage.BlkputRet) {
-        bar.Add64(int64(blkSize))
-    }
+	notifyCallback := func(blkIdx int, blkSize int, ret *storage.BlkputRet) {
+		bar.Add64(int64(blkSize))
+	}
 
-    extra := &storage.RputExtra{
-        TryTimes: 1000,
-        Notify:   notifyCallback,
-    }
+	extra := &storage.RputExtra{
+		TryTimes: 1000,
+		Notify:   notifyCallback,
+	}
 
-    uploader := storage.NewResumeUploader(conf)
-    if err := uploader.Put(context.Background(), nil, token, filename, f, stat.Size(), extra); err != nil {
-        log.Fatal(err)
-    }
-    bar.Finish()
+	uploader := storage.NewResumeUploader(conf)
+	if err := uploader.Put(context.Background(), nil, token, filename, f, stat.Size(), extra); err != nil {
+		log.Fatal(err)
+	}
+	bar.Finish()
 
-    if !strings.HasSuffix(Domain, "/") {
-        Domain = Domain + "/"
-    }
-    dURL, err := url.Parse(Domain + filename)
-    if err != nil {
-        log.Fatal(err)
-    }
-    if dURL.Scheme == "" {
-        dURL.Scheme = "http"
-    }
-    _ = clipboard.WriteAll(dURL.String())
-    fmt.Println("✅ Download URL (already in clipboard): " + dURL.String())
+	if !strings.HasSuffix(Domain, "/") {
+		Domain = Domain + "/"
+	}
+	dURL, err := url.Parse(Domain + filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if dURL.Scheme == "" {
+		dURL.Scheme = "http"
+	}
+	_ = clipboard.WriteAll(dURL.String())
+	fmt.Println("✅ Download URL (already in clipboard): " + dURL.String())
 }
